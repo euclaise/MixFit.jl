21584445c989f0caefb9ce949e646fb22e7f6abf
The-Sushi <falsifiable101@gmail.com>
2020-03-11 20:19:49 -0700

    please work

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 using Documenter, MixFit
 
 makedocs(sitename="MixFit Documentation", modules = [MixFit])
-deploydocs(repo = "https://github.com/the-sushi/MixFit.jl.git")
+deploydocs(repo = "github.com/the-sushi/MixFit.jl.git")
